143159b4bc1a2e5028411049728fb019b8e244af
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-06-12 17:04:08 +0200

    Last commit

---
+++ index.ts
@@ -0,0 +1,81 @@
+
+// string
+
+function digaOla(nome: string): string {
+    return "Olá, " + nome;
+  }
+  
+  console.log(digaOla("Fernando"));
+  
+  // number
+  
+  function soma(n1: number, n2: number): number {
+    return n1 + n2;
+  }
+  
+  console.log(soma(5, 6));
+  
+  // boolean
+  
+  function maiorQue(n1: number, n2: number): boolean {
+    return n1 > n2;
+  }
+  
+  console.log(maiorQue(2, 4));
+  
+  // null
+  
+  function retornaNulo(): null {
+    return null;
+  }
+  const resultado = retornaNulo();
+  console.log(resultado);
+  
+  
+  // undefined
+  function retornaIndefinido(): undefined {
+    return undefined;
+  }
+  const resultadO = retornaIndefinido();
+  console.log(resultado);
+  
+  
+  // array
+  
+  // operador rest/spread
+  function agrupaNomes(...nomes: string[]): string[] {
+    return nomes;
+  }
+  
+  console.log(agrupaNomes("Fred", "Fernando", "Carlos"));
+  
+  // any
+  
+  function qualquerCoisa(qualquer: any): any {
+    return qualquer;
+  }
+  
+  console.log(qualquerCoisa(3));
+  
+  // object
+  
+  function montaObjeto(propriedade: string, valor: any): object {
+    return { [propriedade]: valor }
+  }
+  
+  console.log(montaObjeto("numero", 3));
+  
+  // enum
+  
+  enum Cores {
+    VERMELHA = 1,
+    PRETO = 2,
+    AMARELO = 3,
+    BRANCO = 4,
+  }
+  
+  function codigoCor(cor: string): number {
+    return Cores[cor];
+  }
+  
+  console.log(codigoCor("BRANCO"));
